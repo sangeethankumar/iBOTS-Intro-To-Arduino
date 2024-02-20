b5038acfe8cb2391b879152a09f6b981a89db463
sangeethankumar <an.sangeetha@gmail.com>
2024-02-20 14:15:21 +0100

    blue

--- Day2/Session1/src/main.cpp
+++ Day2/Session1/src/main.cpp
@@ -4,7 +4,7 @@
 // [x] View the simulated Arduino by opening the diagram.json file (shows up when PlatformIO and WokWi Simulator extensions are installed).
 // [x] The Red LED is turned on.
 // [x] The Green LED is also turned on.
-// [ ] The Blue LED is also turned on.
+// [x] The Blue LED is also turned on.
 // [ ] 300 milliseconds after all the LEDs turn on, the Blue LED turns off.
 // [ ] The Red LED then turns on and off every second, forever.
 // [ ] The Green LED simultaneously turns on and off every half-second, forever.
@@ -23,9 +23,9 @@ void setup() {
     pinMode(4, OUTPUT);
     digitalWrite(4, HIGH);
 
-    // green
-    // pinMode(4, OUTPUT);
-    // digitalWrite(4, HIGH);
+    // purple
+    pinMode(5, OUTPUT);
+    digitalWrite(5, HIGH);
 
 }
 

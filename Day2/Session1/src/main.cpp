4a3712c4efe6bbe9b3d3a1a9778cfab59e7a9d6b
sangeethankumar <an.sangeetha@gmail.com>
2024-02-20 14:14:27 +0100

    green

--- Day2/Session1/src/main.cpp
+++ Day2/Session1/src/main.cpp
@@ -1,9 +1,9 @@
 // Development Plan:  Check off each step with an 'x' as you complete each one:
 // [x] Showed up to the workshop.
-// [ ] This file is open in VSCode.
-// [ ] View the simulated Arduino by opening the diagram.json file (shows up when PlatformIO and WokWi Simulator extensions are installed).
-// [ ] The Red LED is turned on.
-// [ ] The Green LED is also turned on.
+// [x] This file is open in VSCode.
+// [x] View the simulated Arduino by opening the diagram.json file (shows up when PlatformIO and WokWi Simulator extensions are installed).
+// [x] The Red LED is turned on.
+// [x] The Green LED is also turned on.
 // [ ] The Blue LED is also turned on.
 // [ ] 300 milliseconds after all the LEDs turn on, the Blue LED turns off.
 // [ ] The Red LED then turns on and off every second, forever.
@@ -15,6 +15,17 @@
 #include <Arduino.h>
 
 void setup() {
+    // red
+    pinMode(3, OUTPUT);
+    digitalWrite(3, HIGH);
+
+    // green
+    pinMode(4, OUTPUT);
+    digitalWrite(4, HIGH);
+
+    // green
+    // pinMode(4, OUTPUT);
+    // digitalWrite(4, HIGH);
 
 }
 
